cc7c69beaa9efc0315dd827db8a5ff2a51c65aa4
Eugene Zvedenyuk <webix@Eugenes-MacBook-Pro.local>
2020-04-20 10:53:19 +0300

    Added language dictionary. Started creating initial logic

--- bot.py
+++ bot.py
@@ -2,7 +2,10 @@
 import sys
 import os
 import time
+import logging
 from botConfig import *
+from cards import *
+from language import *
 import telebot
 from telebot import apihelper
 reload(sys)
@@ -10,18 +13,37 @@ sys.setdefaultencoding('utf-8')
 
 appDir=os.path.abspath(os.path.dirname(__file__))
 
+logger = logging.getLogger('GoC')
+c_handler = logging.StreamHandler()
+c_format = logging.Formatter('%(asctime)s %(levelname)-7s %(funcName)15s:%(lineno)3d ☄️  %(message)s',"%d.%m %H:%M:%S")
+c_handler.setFormatter(c_format)
+logger.addHandler(c_handler)
+logger.setLevel(logging.DEBUG)
+
 apihelper.proxy = telebotProxy
 bot = telebot.TeleBot(botToken)
 
 @bot.message_handler(content_types=['text'])
 def main(message):
+    if not os.path.exists(appDir+"/db/users/"+str(message.from_user.id)+".json"):
+        bot.send_message(message.chat.id,txt["default"]["chooseLanguage"],parse_mode= "Markdown")
+        bot.register_next_step_handler(message, hello)
     if message.text=="/hey":
         print "ping"
         bot.send_message(message.chat.id,"*Hi!*",parse_mode="Markdown")
+
+def hello(message):
+    print message.from_user.id
+    print message.chat.id
+    if message.text=="/eng" or  message.text=="/rus":
+        bot.send_message(message.chat.id,message.from_user.id)
+    else:
+        bot.send_message(message.chat.id,txt["default"]["chooseLanguage"],parse_mode= "Markdown")
+        bot.register_next_step_handler(message, hello)
 
 while True:
 	try:
 		bot.polling(none_stop=True,interval=5,timeout=100)
 	except Exception as e:
-		print e
+		logger.error(e)
 		time.sleep(15)
---
+++ language.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+txt={
+    "default":{
+        "chooseLanguage":"*Choose language:*\n\n/eng — English\n/rus — Русский"
+    },
+    "ru":{
+        "chooseWinnerOrSkip":"Сначала выберите победителя или пропустите карту",
+    },
+    "en":{
+        "chooseWinnerOrSkip":"You need to choose winner or skip",
+    }
+}
