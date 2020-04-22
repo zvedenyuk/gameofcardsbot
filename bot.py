869457cc57a2cc1980bb064d15e88ed7f27102c8
Eugene Zvedenyuk <webix@Eugenes-MacBook-Pro.local>
2020-04-22 18:26:04 +0300

    Added nicknames functionality

--- bot.py
+++ bot.py
@@ -72,25 +72,21 @@ def main(message):
 	db.load_game()
 	try: db.user["lang"]
 	except KeyError:
-		bot.send_message(message.chat.id,"*log1* "+str(message.text),parse_mode="Markdown")
 		msg("chooseLanguage",lang="default")
 		bot.register_next_step_handler(message, change_language)
 		return False
-	if message.text=="/hey":
-		print "ping"
-		bot.send_message(message.chat.id,"*Hi!*",parse_mode="Markdown")
-	elif db.user["step"]=="main":
+	if db.user["step"]=="main":
 		if message.text=="/create":
 			room=44
 			while os.path.exists(appDir+"/db/games/"+str(room)+".json"):
 				room=randint(10000,99999)
 			db.user["step"]="created"
 			db.user["room"]=str(room)
-			db.game={"admin":db.userId,"players":[db.userId],"status":"preparing"}
+			db.game={"admin":db.userId,"players":[db.userId],"nicknames":{},"status":"preparing"}
 			db.save_game()
 			db.save_user()
-			msg("create",room)
-			msg("created")
+			msg("chooseName")
+			bot.register_next_step_handler(message, choose_name)
 		elif message.text=="/join":
 			msg("join")
 			bot.register_next_step_handler(message, room_join)
@@ -105,7 +101,7 @@ def main(message):
 			db.save_game()
 			for player in db.game["players"]:
 				db.game["status"]="started"
-				msg("gameStarted","\n".join(str(v) for v in db.game["players"]))
+				msg("gameStarted","\n".join(str(db.game["nicknames"][str(v)]) for v in db.game["players"]))
 		elif message.text=="/cancel":
 			db.user["step"]="main"
 			db.user["room"]=""
@@ -114,7 +110,10 @@ def main(message):
 		else:
 			msg("created")
 	elif db.user["step"]=="joined":
-		msg("joined")
+		if db.game["status"]=="started":
+			pass
+		else:
+			msg("joinedWait")
 
 def change_language(message):
 	if message.text=="/en" or message.text=="/ru":
@@ -123,7 +122,6 @@ def change_language(message):
 		db.save_user()
 		msg("hello")
 	else:
-		bot.send_message(message.chat.id,"*log2* "+message.text,parse_mode="Markdown")
 		msg("chooseLanguage",lang="default")
 		bot.register_next_step_handler(message, change_language)
 
@@ -141,8 +139,22 @@ def room_join(message):
 			db.load_game()
 			db.game["players"].append(message.from_user.id)
 			db.save_game()
-			msg("adminJoined",text="@"+str(message.from_user.username),chat=db.game["admin"])
-			msg("joined",message.text)
+			msg("chooseName")
+			bot.register_next_step_handler(message, choose_name)
+
+def choose_name(message):
+	db.user["nickname"]=str(message.text)
+	db.game["nicknames"][db.userId]=str(message.text)
+	db.save_user()
+	db.save_game()
+	if db.user["step"]=="joined":
+		msg("adminJoined",text=db.user["nickname"],chat=db.game["admin"])
+		msg("joined",db.user["room"])
+		msg("joinedWait")
+	if db.user["step"]=="created":
+		msg("create",db.user["room"])
+		msg("created")
+
 
 if __name__ == "__main__":
 	db=Db()
